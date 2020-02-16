1146a4259fe26548fe11a485ec99b6474601da14
andrew4ever <andrewyazura203@gmail.com>
2020-02-16 23:23:32 +0200

    Tests for allUsers and findUser

---
+++ test/services/users/allUsers.test.js
@@ -0,0 +1,43 @@
+import accountModel from '../../../src/models/accountModel.js';
+import allUsers from '../../../src/services/users/allUsers';
+import userModel from '../../../src/models/userModel.js';
+import setupDB from '../../setupDatabase';
+
+setupDB('all-users-test');
+
+describe('allUsers', () => {
+  test('single user', async () => {
+    await userModel.create({
+      name: 'John',
+      email: 'john@404.com',
+      password: '123',
+    });
+
+    const obj = {
+      name: 'John',
+      email: 'john@404.com',
+    };
+
+    const users = await allUsers();
+
+    expect(users[0].toObject()).toMatchObject(obj);
+  });
+
+  test('multiple users', async () => {
+    await userModel.create({
+      name: 'John',
+      email: 'john@404.com',
+      password: '123',
+    });
+
+    await userModel.create({
+      name: 'Jonny',
+      email: 'jonny@404.com',
+      password: '1234',
+    });
+
+    const users = await allUsers();
+
+    expect(users.length).toBe(2);
+  });
+});
