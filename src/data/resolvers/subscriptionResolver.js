9b916a0f29a23dc64f49f4ef4a7711f3a474437c
andrew4ever <andrewyazura203@gmail.com>
2020-03-16 21:27:13 +0200

    Fix subscriptions bug

--- src/data/resolvers/subscriptionResolver.js
+++ src/data/resolvers/subscriptionResolver.js
@@ -2,7 +2,7 @@ import { subscriptions } from '../../config';
 
 const subscriptionResolver = {
   subscriptions: async (args, request) => {
-    subscriptions.pop();
+    subscriptions.slice(0, -1);
     return subscriptions;
   },
 };
